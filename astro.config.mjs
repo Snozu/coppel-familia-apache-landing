8c05d5de9d62d674e7adb919389fd566362769a7
Snozu <hazieleduardo@gmail.com>
2025-06-27 10:39:34 -0600

    build: update dependencies and Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  integrations: [
+    partytown({
+      // Configuración para Google Analytics
+      config: {
+        forward: ["dataLayer.push", "gtag"],
+      },
+    }),
+  ],
 })
